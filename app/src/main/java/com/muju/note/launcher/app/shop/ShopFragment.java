71ab61fab89c37d7d332667fbf49483403c0b218
lengyang <18875235540@163.com>
2019-06-20 15:29:30 +0800

    新增购物等跳转空白页

--- app/src/main/java/com/muju/note/launcher/app/shop/ShopFragment.java
+++ app/src/main/java/com/muju/note/launcher/app/shop/ShopFragment.java
@@ -1,19 +1,36 @@
 package com.muju.note.launcher.app.shop;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.muju.note.launcher.R;
+import com.muju.note.launcher.app.publicui.NullFragment;
 import com.muju.note.launcher.base.BaseFragment;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class ShopFragment extends BaseFragment {
     @BindView(R.id.tv_title)
     TextView tvTitle;
     @BindView(R.id.rel_titlebar)
     RelativeLayout relTitlebar;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.tv_back)
+    TextView tvBack;
+    @BindView(R.id.ll_back)
+    LinearLayout llBack;
+    @BindView(R.id.ll_main)
+    LinearLayout llMain;
 
     @Override
     public int getLayout() {
@@ -24,6 +41,13 @@ public class ShopFragment extends BaseFragment {
     public void initData() {
         relTitlebar.setBackgroundColor(getResources().getColor(R.color.white));
         tvTitle.setText("严选好物");
+
+        llMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start(new NullFragment());
+            }
+        });
     }
 
     @Override
