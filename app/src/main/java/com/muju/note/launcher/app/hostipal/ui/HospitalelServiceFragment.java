9f37ceb67f24308c8e09c003ec96d6eca9232e56
lengyang <18875235540@163.com>
2019-07-19 17:55:22 +0800

    医疗服务首页修改

--- app/src/main/java/com/muju/note/launcher/app/hostipal/ui/HospitalelServiceFragment.java
+++ app/src/main/java/com/muju/note/launcher/app/hostipal/ui/HospitalelServiceFragment.java
@@ -1,81 +1,58 @@
 package com.muju.note.launcher.app.hostipal.ui;
 
 
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.muju.note.launcher.R;
-import com.muju.note.launcher.app.hostipal.adapter.ServiceAdapter;
-import com.muju.note.launcher.app.hostipal.bean.Entity;
 import com.muju.note.launcher.base.BaseFragment;
-import com.muju.note.launcher.base.LauncherApplication;
-import com.muju.note.launcher.util.toast.FancyToast;
-import com.muju.note.launcher.view.SpaceItemDecoration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
-import butterknife.OnClick;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
-//医院服务
-public class HospitalelServiceFragment extends BaseFragment {
+/**
+ * 医疗服务
+ */
+public class HospitalelServiceFragment extends BaseFragment implements View.OnClickListener {
     private static final String TAG = "HospitalelEganceFragment";
-    @BindView(R.id.ll_back)
-    LinearLayout llBack;
     @BindView(R.id.tv_title)
     TextView tvTitle;
-    @BindView(R.id.rel_titlebar)
-    RelativeLayout relTitlebar;
-    @BindView(R.id.rl)
-    RecyclerView rl;
-    private ServiceAdapter serviceAdapter;
+    @BindView(R.id.ll_cost)
+    LinearLayout llCost;
+    @BindView(R.id.btn_cost1)
+    Button btnCost1;
+    @BindView(R.id.btn_cost2)
+    Button btnCost2;
+    @BindView(R.id.btn_cost3)
+    Button btnCost3;
+    @BindView(R.id.btn_cost4)
+    Button btnCost4;
+    @BindView(R.id.btn_cost5)
+    Button btnCost5;
+    @BindView(R.id.ll_chemotherapay)
+    LinearLayout llChemotherapay;
 
 
     @Override
     public int getLayout() {
-        return R.layout.fragment_service_navi;
+        return R.layout.fragment_new_his_service;
     }
 
     @Override
     public void initData() {
-        tvTitle.setText("医院服务");
-        relTitlebar.setBackgroundColor(getResources().getColor(R.color.white));
-        initServices();
-    }
-
-
-    private void initServices() {
-        rl.setLayoutManager(new GridLayoutManager(LauncherApplication.getInstance(), 5));
-        final List<Entity> data = new ArrayList<>();
-
-        data.add(new Entity(R.mipmap.icon_cost_query, null));
-        data.add(new Entity(R.mipmap.icon_laboratory_notice, null));
-        data.add(new Entity(R.mipmap.icon_examination_report, null));
-        data.add(new Entity(R.mipmap.icon_be_careful, null));
-        data.add(new Entity(R.mipmap.icon_drug_use, null));
-        serviceAdapter = new ServiceAdapter(data);
-
-        rl.addItemDecoration(new SpaceItemDecoration(32, 0, 146, 0));
-        rl.setAdapter(serviceAdapter);
-        serviceAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                if (position == 0) {
-                    start(new CostFragment());
-                } else if (position == 2) {
-                    start(new ChemotherapyFragment());
-                } else {
-                    FancyToast.makeText(LauncherApplication.getInstance(), "更多功能,敬请期待", FancyToast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
+        llCost.setOnClickListener(this);
+        btnCost1.setOnClickListener(this);
+        btnCost2.setOnClickListener(this);
+        btnCost3.setOnClickListener(this);
+        btnCost4.setOnClickListener(this);
+        btnCost5.setOnClickListener(this);
+        llChemotherapay.setOnClickListener(this);
     }
 
     @Override
@@ -89,11 +66,24 @@ public class HospitalelServiceFragment extends BaseFragment {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_cost:
+                start(new CostFragment());
+                break;
+            case R.id.btn_cost1:
+            case R.id.btn_cost2:
+            case R.id.btn_cost3:
+            case R.id.btn_cost4:
+            case R.id.btn_cost5:
+                start(new CostFragment());
+                break;
 
-    @OnClick(R.id.ll_back)
-    public void onViewClicked() {
-        pop();
+            case R.id.ll_chemotherapay:
+                start(new ChemotherapyFragment());
+                break;
+        }
     }
-
 
 }
