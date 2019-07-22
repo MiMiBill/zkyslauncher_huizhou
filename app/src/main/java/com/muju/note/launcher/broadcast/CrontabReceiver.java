ac90b7e9ef18937087652dce36c3074c0f2863f7
mingpingkun <19925330202@189.cn>
2019-07-22 09:48:02 +0800

    修复定时事件广播为空的问题

--- app/src/main/java/com/muju/note/launcher/broadcast/CrontabReceiver.java
+++ app/src/main/java/com/muju/note/launcher/broadcast/CrontabReceiver.java
@@ -21,27 +21,29 @@ public class CrontabReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 //        LogFactory.l().i("onReceive==="+intent.getAction());
-        if (intent.getAction().equals(Constants.CRONTAB_ACTION_SINGLE)) {   //单次事件
-            id = intent.getStringExtra("id");
-            EventBus.getDefault().post(new PushCustomMessageEntity(id));
-        }else if(intent.getAction().equals(Constants.CRONTAB_ACTION_DAY)){  //每天
-            id = intent.getStringExtra("id");
-            EventBus.getDefault().post(new PushCustomMessageEntity(id));
-        }else if(intent.getAction().equals(Constants.CRONTAB_ACTION_WEEK)){ //自定义week
-            id = intent.getStringExtra("id");
-            remind_number = intent.getStringExtra("remind_number");
-            LogFactory.l().i("remind_number==="+remind_number);
-            String[] arrr = remind_number.split(",");
-            Calendar cc = Calendar.getInstance();
-            int weeks = cc.get(Calendar.DAY_OF_WEEK);
-            String Tweeks = weeks + "";
-            LogFactory.l().i("Tweeks==="+Tweeks);
-            for (int i = 0; i < arrr.length; i++) {
-                if (Integer.valueOf(arrr[i])==0){
-                    arrr[i]="7";
-                }
-                if (arrr[i].equals(Tweeks)) {
-                    EventBus.getDefault().post(new PushCustomMessageEntity(id));
+        if(intent.getAction()!=null){
+            if (intent.getAction().equals(Constants.CRONTAB_ACTION_SINGLE)) {   //单次事件
+                id = intent.getStringExtra("id");
+                EventBus.getDefault().post(new PushCustomMessageEntity(id));
+            }else if(intent.getAction().equals(Constants.CRONTAB_ACTION_DAY)){  //每天
+                id = intent.getStringExtra("id");
+                EventBus.getDefault().post(new PushCustomMessageEntity(id));
+            }else if(intent.getAction().equals(Constants.CRONTAB_ACTION_WEEK)){ //自定义week
+                id = intent.getStringExtra("id");
+                remind_number = intent.getStringExtra("remind_number");
+                LogFactory.l().i("remind_number==="+remind_number);
+                String[] arrr = remind_number.split(",");
+                Calendar cc = Calendar.getInstance();
+                int weeks = cc.get(Calendar.DAY_OF_WEEK);
+                String Tweeks = weeks + "";
+                LogFactory.l().i("Tweeks==="+Tweeks);
+                for (int i = 0; i < arrr.length; i++) {
+                    if (Integer.valueOf(arrr[i])==0){
+                        arrr[i]="7";
+                    }
+                    if (arrr[i].equals(Tweeks)) {
+                        EventBus.getDefault().post(new PushCustomMessageEntity(id));
+                    }
                 }
             }
         }
